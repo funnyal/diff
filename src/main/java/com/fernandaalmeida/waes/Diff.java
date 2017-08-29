d3a103129fe5c88ebbfe06997dded7c522cb8272
Fernanda Almeida <fernanda.vbalmeida@gmail.com>
2017-08-29 12:05:04 +0100

    Adding method diff

--- src/main/java/com/fernandaalmeida/waes/Diff.java
+++ src/main/java/com/fernandaalmeida/waes/Diff.java
@@ -1,23 +1,89 @@
 package com.fernandaalmeida.waes;
 
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import io.vertx.core.json.JsonObject;
 
 public class Diff {
-	
+
 	private JsonObject left;
 	private JsonObject right;
-	
+
 	public JsonObject getLeft() {
 		return left;
 	}
+
 	public void setLeft(JsonObject left) {
 		this.left = left;
 	}
+
 	public JsonObject getRight() {
 		return right;
 	}
+
 	public void setRight(JsonObject right) {
 		this.right = right;
+	}
+
+	public String checkDiff() {
+		// for( left.)
+		return null;
+	}
+
+	public String varre(JSONObject json, JSONObject json2) {
+
+		String finalRes = "";
+		try {
+			String left = "";
+			String right = "";
+			Iterator<String> temp = json.keys();
+			Iterator<String> temp2 = json2.keys();
+			if (json.similar(json2)) {
+				System.out.println("similar");
+				return "similar";
+			} else {
+				while (temp.hasNext()) {
+					String key = temp.next();
+					Object value = json.get(key);
+					if (json2.has(key)) {
+						// String value2 = json2.getString(key); instafeof array
+						String value2 = json2.getString(key);
+						if (!value.equals(value2)) {
+							left += key + "=" + value2;
+						}
+					} else {
+						left += key + "=" + value;
+					}
+
+				}
+			}
+			left = "on left : " + left;
+			while (temp2.hasNext()) {
+				String key2 = temp2.next();
+				Object value2 = json2.get(key2);
+				if (!finalRes.contains(key2)) {
+					if (json.has(key2)) {
+						// String value2 = json2.getString(key); instafeof array
+						String value1 = json.getString(key2);
+						if (!value2.equals(value1)) {
+							right += key2 + "=" + value1;
+						}
+					} else {
+						right += key2 + "=" + value2;
+					}
+				}
+
+			}
+			right = "on right : " + right;
+			System.out.println(left + right);
+			finalRes = left + right;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return finalRes;
 	}
 
 }
--- src/main/java/com/fernandaalmeida/waes/WAESVerticle.java
+++ src/main/java/com/fernandaalmeida/waes/WAESVerticle.java
@@ -2,14 +2,11 @@ package com.fernandaalmeida.waes;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.FileUpload;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -30,27 +27,12 @@ public class WAESVerticle extends AbstractVerticle {
 
 		    vertx.createHttpServer().requestHandler(router::accept).listen(8080, result -> {
 	          if (result.succeeded()) {
-	        	  System.out.println("ccc");
 	            fut.complete();
 	          } else {
-	        	  System.out.println("dddd");
 	            fut.fail(result.cause());
 	          }
 	        });
 		    
-		    
-//		    vertx
-//	        .createHttpServer()
-//	        .requestHandler(r -> {
-//	          r.response().end("<h1>Hello WAES</h1>");
-//	        })
-//	        .listen(8080, result -> {
-//	          if (result.succeeded()) {
-//	            fut.complete();
-//	          } else {
-//	            fut.fail(result.cause());
-//	          }
-//	        });
 	  }
 	 
 	 private void handleLeft(RoutingContext rc){
@@ -66,8 +48,29 @@ public class WAESVerticle extends AbstractVerticle {
 	      rc.response()
 	          .putHeader("content-type", "application/json")
 	          .putHeader("X-Version","v1")
-	          .end(new JsonObject().put("greeting", "Hello World!").encode());
+	          .end(new JsonObject().put("server", "OK").encode());
 	 }
 	 
+	 private void handleRight(RoutingContext rc){
+		 Buffer buffer = rc.getBody();
+		 JsonObject requestBufferJson = new JsonObject(buffer.toString());
+		 Integer id = new Integer(rc.request().getParam("id"));
+		 
+		 Diff diffObj = new Diff();
+		 diffObj.setRight(requestBufferJson);
+		 
+		 jsons.put(id, diffObj);
+		
+	      rc.response()
+	          .putHeader("content-type", "application/json")
+	          .putHeader("X-Version","v1")
+	          .end(new JsonObject().put("greeting", "Hello WorldRight!").encode());
+	 }
 	 
+	 private void returnDiff(RoutingContext rc){
+
+		 Integer id = new Integer(rc.request().getParam("id"));
+		 
+		 Diff diffObj = jsons.get(id);
+	 }
 }
